379d8364b450297fcb7cebd3df16551d56fefdd3
ApoTheOne <anurag.saklani4@gmail.com>
2018-02-18 16:13:25 +0530

    Converted Linked List to Generic

--- LinkedList.cs
+++ LinkedList.cs
@@ -1,20 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 namespace Revision_DS_Algo
 {
-    public class LinkedList
+    public class LinkedList<T>
     {
-        public LinkedListNode Head { get; set; }
-        public LinkedListNode Tail { get; set; }
+        public LinkedListNode<T> Head { get; set; }
+        public LinkedListNode<T> Tail { get; set; }
         public int Count { get; set; }
         public LinkedList()
         {
             Head = Tail;
             Count = 0;
         }
-        public void AddFirst(LinkedListNode node)
+        public void AddFirst(LinkedListNode<T> node)
         {
-            LinkedListNode temp = Head;
+            LinkedListNode<T> temp = Head;
+            Head = node;
             Head.Next = temp;
             Count++;
             if(Count == 1)
@@ -23,7 +25,7 @@ namespace Revision_DS_Algo
             }
         }
 
-        public void AddLast(LinkedListNode node)
+        public void AddLast(LinkedListNode<T> node)
         {
             if(Count == 0)
             {
@@ -36,14 +38,27 @@ namespace Revision_DS_Algo
             Tail = node;
             Count++;
         }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = Head;
+            while(current.Next != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
         
     }
 
-    public class LinkedListNode
+    public class LinkedListNode<T>
     {
-        public int value { get; set; }
-        public LinkedListNode Next { get; set; }
+        public LinkedListNode(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
     }
 
-    
 }
--- Program.cs
+++ Program.cs
@@ -6,20 +6,33 @@ namespace Revision_DS_Algo
     {
         static void Main(string[] args)
         {
-            Node first = new Node(){value = 1 };
+            Node first = new Node() { value = 1 };
 
-            Node second = new Node(){value = 2};
+            Node second = new Node() { value = 2 };
             first.Next = second;
 
-            Node three = new Node() { value = 3};
+            Node three = new Node() { value = 3 };
             second.Next = three;
 
             PrintList(first);
+
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddFirst(new LinkedListNode<int>(1));
+            linkedList.AddLast(new LinkedListNode<int>(2));
+            linkedList.AddLast(new LinkedListNode<int>(3));
+            try
+            {
+                var linkList = linkedList.GetEnumerator();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
         }
 
         private static void PrintList(Node node)
         {
-            while(node != null)
+            while (node != null)
             {
                 Console.WriteLine(node.value);
                 node = node.Next;
